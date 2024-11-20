3ea60abe4efa2465b521c8213ed79eb8e0589edb
Gabriel Tavares Barbosa <gabrieltavares.tic@gmail.com>
2024-11-19 21:55:57 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gabriel Barbosa on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
